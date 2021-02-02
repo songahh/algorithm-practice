d67ae90dbc873481630fcc75ec7ec9a58180fdf6
songahh <gunsong2@gmail.com>
2021-02-02 21:21:40 +0900

    풀이2,3 - reverse(), swap 이용

---
+++ ch6/02/Reverse_String_02.py
@@ -0,0 +1,12 @@
+"""
+    문자열을 뒤집는 함수를 작성하라. 입력값은 문자 배열이며, 리턴 없이 리스트 내부를 직접 조작하라.
+    Do not allocate extra space for another array,
+    you must do this by modifying the input array in-place with O(1) extra memory.
+
+    tc1: ["h","e","l","l","o"]
+    tc2: ["H","a","n","n","a","h"]
+"""
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
